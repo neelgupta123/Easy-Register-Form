bf2ca80cbc28a9d2dcd292cce11618d26683e0c7
neelgupta <guptaneelhome@gmail.com>
2021-04-28 19:23:40 +0530

    Done Full Code

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
-$sn = "localhost";
-$un = "root";
-$pw = "";
+$servername = "localhost";
+$username = "root";
+$password = "";
 // You have to make a Database and Name it here.
-$db = "register";
+$dbname = "register";
 
-$conn = mysqli_connect($sn, $un, $pw, $db);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 
-if(!$conn){
-    echo("There is an error -->". mysqli_connect_error());
+if (!$conn) {
+    die("There is an error -->" . mysqli_connect_error());
 }
 
 $name_1 = $_POST['1_name'];
@@ -17,6 +17,15 @@ $email = $_POST['email'];
 $password = $_POST['password'];
 $conpassword = $_POST['confirm_password'];
 
+
 // Give Custom table Name.
 
 $sql = "INSERT INTO `user_reg` (`name1`, `name2`, `email`, `pass`, `conpass`) VALUES ('$name_1', '$name_2', '$email', '$password', '$conpassword');";
+
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+  } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
+  
+  $conn->close();
